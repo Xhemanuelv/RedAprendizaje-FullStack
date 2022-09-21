06590164981ae80d46aa92a85834c57aef1cbdd9
Xhemanuelv <xhemanuelv@gmail.com>
2022-09-21 19:52:06 -0300

    Cuarto commit

---
+++ src/javaintro/JavaIntroEj07.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author xhemanuelv
+ */
+public class JavaIntroEj07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Considera que estás desarrollando una web para una empresa que fabrica motores (suponemos
+    que se trata del tipo de motor de una bomba para mover fluidos). Definir una variable tipoMotor y
+    permitir que el usuario ingrese un valor entre 1 y 4. El programa debe mostrar lo siguiente:
+    o Si el tipo de motor es 1, mostrar un mensaje indicando “La bomba es una bomba de agua”.
+    o Si el tipo de motor es 2, mostrar un mensaje indicando “La bomba es una bomba de
+    gasolina”.
+    o Si el tipo de motor es 3, mostrar un mensaje indicando “La bomba es una bomba de
+    hormigón”.
+    o Si el tipo de motor es 4, mostrar un mensaje indicando “La bomba es una bomba de pasta
+    alimenticia”.
+    o Si no se cumple ninguno de los valores anteriores mostrar el mensaje “No existe un valor
+    válido para tipo de bomba”
+        */
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Bienvenido a <enterprise_name_here>");
+        System.out.println("Seleccione el tipo de bomba que desea adquirir");
+        System.out.println("1 - Bomba de agua");
+        System.out.println("2 - Bomba de gasolina");
+        System.out.println("3 - Bomba de hormigón");
+        System.out.println("4 - Bomba de pasta alimenticia");
+        int tipoMotor = leer.nextInt();
+        if(tipoMotor>4 && tipoMotor<1){
+            System.out.println("No existe un valor válidoo para tipo de bomba");
+        }else if(tipoMotor<=4&&tipoMotor>=1){
+            switch (tipoMotor){
+                case 1:
+                    System.out.println("La bomba es una bomba de agua");
+                    break;
+                case 2:
+                    System.out.println("La bomba es una bomba de gasolina");
+                    break;
+                case 3:
+                    System.out.println("La bomba es una bomba de hormigón");
+                    break;
+                case 4:
+                    System.out.println("La bomba es una bomba de pasta alimenticia");
+                    break;  
+            }
+        }
+    }
+}
