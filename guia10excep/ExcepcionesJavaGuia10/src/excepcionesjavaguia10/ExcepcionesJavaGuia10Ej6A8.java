fb52e7f1c5a12c1457130cda9de7289050cee00d
Xhemanuelv <xhemanuelv@gmail.com>
2022-12-07 22:11:12 -0300

    Relleno para usar merge y pull

---
+++ guia10excep/ExcepcionesJavaGuia10/src/excepcionesjavaguia10/ExcepcionesJavaGuia10Ej6A8.java
@@ -0,0 +1,54 @@
+package excepcionesjavaguia10;
+
+/**
+ *
+ * @author Xhemanuelv
+ */
+public class ExcepcionesJavaGuia10Ej6A8 {
+    
+    /**
+    * 6 Dado el método metodoA de la clase A, indique:
+    * a) ¿Qué sentencias y en qué orden se ejecutan si se produce la excepción MioException?
+    * a1
+    * a2
+    * a6
+    * a6
+    * 
+    * b) ¿Qué sentencias y en qué orden se ejecutan si no se produce la excepción MioException?
+    * a1
+    * a2
+    * a3
+    * a4
+    * 
+    * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    * 
+    * Dado el método metodoB de la clase B, indique:
+    * a) ¿Qué sentencias y en qué orden se ejecutan si se produce la excepción MioException?
+    * b1
+    * b3
+    * b4
+    * 
+    * b) ¿Qué sentencias y en qué orden se ejecutan si no se produce la excepción MioException?
+    * b1
+    * b2
+    * b4
+    * 
+    * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    * 
+    * 8 Indique que se mostrará por pantalla cuando se ejecute cada una de estas clases
+    * 
+    * class Uno
+    * 
+    * - 43
+    * - 44 valor catch
+    * - 45 valor finaly
+    * 
+    * class Dos
+    * 
+    * - 43
+    * - 44 valor catch
+    * - 45 valor finaly
+    * 
+    */
+    
+}
